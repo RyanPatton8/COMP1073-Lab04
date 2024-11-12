545fe7a137bbd696dd7279c59c7dd8d61e922e9d
Ryan Patton <ryanpatton173@gmail.com>
2024-11-12 10:54:43 -0500

    working with my lamp stack url

--- week_10/js/json.js
+++ week_10/js/json.js
@@ -7,7 +7,8 @@ async function populate(){
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
     //const requestURL = "https://ryanpatton8.github.io/COMP1073-Lesson/week_10/js/i-scream.json";
-    const requestURL = "./js/i-scream.json";
+    const requestURL = "https://lamp.computerstudi.es/~Ryan200463597/JavaScript/week_10/js/i-scream.json";
+    //const requestURL = "./js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
     const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
@@ -47,9 +48,11 @@ function showTopFlavors(json) {
         let ul = document.createElement("ul");
 
         // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
+        
         h2.textContent = topFlavors[i]["name"];
         //image.setAttribute("src", `https://ryanpatton8.github.io/COMP1073-Lesson/week_10/images/${topFlavors[i]["image"]}`);
-        image.setAttribute("src", `./images/${topFlavors[i]["image"]}`);
+        image.setAttribute("src", `https://lamp.computerstudi.es/~Ryan200463597/JavaScript/week_10/images/${topFlavors[i]["image"]}`);
+        //image.setAttribute("src", `./images/${topFlavors[i]["image"]}`);
 
         // STEP 10g: Build a loop for the ingredients array in the JSON
         let ingredients = topFlavors[i].ingredients;
