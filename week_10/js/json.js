e4dfebbedcf9f6add94cafb9c5bd4509c0082bb0
Ryan Patton <ryanpatton173@gmail.com>
2024-11-12 10:03:35 -0500

    f3

--- week_10/js/json.js
+++ week_10/js/json.js
@@ -3,23 +3,22 @@ const header = document.querySelector("header");
 const section = document.querySelector("section");
 
 // STEP 3a: Create the asynchronous function populate()
-
+async function populate(){
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    
+    const requestURL = "https://ryanpatton8.github.io/COMP1073-Lesson/week_10/js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
-    
+    const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
+    const response = await fetch(request);
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    
+    const iScream = await response.json();
     // STEP 8: Output the iScream JSON object to the console 
-    
+    console.log(iScream);
     // STEP 9a: Invoke the populateHeader function here, then build it below
     
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
-
+}  
 
 // STEP 3b: Call the populate() function
 
